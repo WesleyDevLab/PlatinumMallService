a3127ef4a0e09752b44179ea7b32db2baf24fa97
MontaserQasem <montaser.tutu@gmail.com>
2016-12-09 04:48:04 +0200

    Finishing the request functionality

---
+++ src/main/java/Plat/Hibernate/Entities/Log.java
@@ -0,0 +1,13 @@
+package Plat.Hibernate.Entities;
+
+/**
+ * Created by MontaserQasem on 12/9/16.
+ */
+public class Log {
+    private int id;
+    private long deliveryData;
+    private Users user;
+    private Orders order;
+    private Admins admin;
+
+}
--- src/main/java/Plat/Hibernate/Services/OrdersAPI.java
+++ src/main/java/Plat/Hibernate/Services/OrdersAPI.java
@@ -5,10 +5,7 @@ import Plat.Hibernate.Util.*;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by MontaserQasem on 11/23/16.
@@ -34,7 +31,7 @@ public class OrdersAPI {
 
     @GET
     @Path("/{orderId}")
-    public Orders getOrderByOrderId(@PathParam("orderId") int id) {
+    public Orders getOrderByOrderId(@PathParam("orderId") long id) {
         RuleObject rule = new RuleObject("id", HibernateUtil.EQUAL, id);
         List<DataBaseObject> objects = manager.find(rule, Orders.class);
         if (objects != null && objects.size() > 0) {
@@ -46,9 +43,9 @@ public class OrdersAPI {
     }
 
     @GET
-    @Path("/{operation}/{storeId}")
-    public List<Orders> getOrdersByOperation(@PathParam("operation") String operation, @PathParam("storeId") int storeId) {
-        if (operation.equalsIgnoreCase("getordersbystoreid")) {
+    @Path("/{operation}/{id}")
+    public List<Orders> getOrdersByOperation(@PathParam("operation") String operation, @PathParam("id") int id) {
+        if (operation.equalsIgnoreCase("getordersbystoreid")) {//gets new requests
             List<Orders> orders = getAllOrders();
             List<Orders> result = new ArrayList<>();
             for (int i = 0; i < orders.size(); i++) {
@@ -58,20 +55,27 @@ public class OrdersAPI {
                 while (it.hasNext()) {
                     OrderItem orderitem = (OrderItem) it.next();
                     int itemId = orderitem.getItem().getId();
-                    if (getItemsStoreId(itemId) == storeId) {
+                    if (getItemsStoreId(itemId) == id) {// id here is equal to storeId
                         flag = true;
                         break;
                     }
                 }
-                if (flag)
+                if (flag && orders.get(i).getStatus() == 1)
                     result.add(orders.get(i));
             }
+            Collections.sort(result, new Comparator<Orders>() {
+                @Override
+                public int compare(Orders o1, Orders o2) {
+                    return (int) (o1.getId() - o2.getId());
+                }
+            });
             return result;
         }
+
         return null;
     }
 
-    public  int getItemsStoreId(int itemId) {
+    public int getItemsStoreId(int itemId) {
         RuleObject rule = new RuleObject("id", HibernateUtil.EQUAL, itemId);
         List<DataBaseObject> object = manager.find(rule, Items.class);
         if (object == null || object.size() == 0) return -1;
@@ -111,6 +115,26 @@ public class OrdersAPI {
     public String updateOrder(Orders order) {
         manager.update(order);
         return "updated";
+    }
+
+    @PUT
+    @Path("{operation}/{orderId}/{adminId}")
+    public String updateOrderState(@PathParam("operation") String operation, @PathParam("orderId") long id, @PathParam("adminId") int adminId) {
+        RuleObject ruleObject = new RuleObject("id", HibernateUtil.EQUAL, id);
+        List<DataBaseObject> object = manager.find(ruleObject, Orders.class);
+        if (object == null || object.size() == 0) return "There's a problem with the orderId";
+        Orders order = (Orders) object.get(0);
+        if (operation.equalsIgnoreCase("accept")) {
+            order.setStatus(3);
+            manager.update(order);
+            return "Request Accepted";
+        }
+        if (operation.equalsIgnoreCase("reject")) {
+            order.setStatus(2);
+            manager.update(order);
+            return "Request Rejected";
+        }
+        return null;
     }
 
     @DELETE
